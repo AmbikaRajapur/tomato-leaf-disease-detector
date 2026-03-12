859d71965e5402e544a53ae27f287d668a48b728
AmbikaRajapur <rajapurambika56@gmail.com>
2026-03-12 12:28:19 +0530

    recommendation.py

--- recommendation.py
+++ recommendation.py
@@ -1,19 +1,14 @@
 solutions = {
 
-"Early_Blight":
-"Remove infected leaves and apply fungicide.",
+"Early_Blight": "Remove infected leaves and apply fungicide.",
 
-"Late_Blight":
-"Use copper fungicide and avoid high humidity.",
+"Late_Blight": "Use copper fungicide and avoid high humidity.",
 
-"Leaf_Mold":
-"Improve air circulation around plants.",
+"Leaf_Mold": "Improve air circulation around plants.",
 
-"Septoria":
-"Remove infected leaves and rotate crops.",
+"Septoria": "Remove infected leaves and rotate crops.",
 
-"Healthy":
-"Leaf is healthy. Maintain proper watering."
+"Healthy": "Leaf is healthy. Maintain proper watering."
 }
 
 def get_solution(disease):
